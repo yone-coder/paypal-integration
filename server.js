e76943cf52858088cdff9bc06f507305fc7a780f
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-07-29 01:37:42 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,182 @@
+import express from "express";
+import "dotenv/config";
+import {
+    ApiError,
+    Client,
+    Environment,
+    LogLevel,
+    OrdersController,
+    PaymentsController,
+} from "@paypal/paypal-server-sdk";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const {
+    PAYPAL_CLIENT_ID,
+    PAYPAL_CLIENT_SECRET,
+    PORT = 8080,
+} = process.env;
+
+const client = new Client({
+    clientCredentialsAuthCredentials: {
+        oAuthClientId: PAYPAL_CLIENT_ID,
+        oAuthClientSecret: PAYPAL_CLIENT_SECRET,
+    },
+    timeout: 0,
+    environment: Environment.Sandbox,
+    logging: {
+        logLevel: LogLevel.Info,
+        logRequest: { logBody: true },
+        logResponse: { logHeaders: true },
+    },
+});
+
+const ordersController = new OrdersController(client);
+const paymentsController = new PaymentsController(client);
+
+const createOrder = async (cart) => {
+    const payload = {
+        body: {
+            intent: "CAPTURE",
+            purchaseUnits: [
+                {
+                    amount: {
+                        currencyCode: "USD",
+                        value: "100",
+                    },
+                },
+            ],
+        },
+        prefer: "return=minimal",
+    };
+
+    try {
+        const { body, ...httpResponse } = await ordersController.createOrder(
+            payload
+        );
+        return {
+            jsonResponse: JSON.parse(body),
+            httpStatusCode: httpResponse.statusCode,
+        };
+    } catch (error) {
+        if (error instanceof ApiError) {
+            throw new Error(error.message);
+        }
+    }
+};
+
+app.post("/api/orders", async (req, res) => {
+    try {
+        const { cart } = req.body;
+        const { jsonResponse, httpStatusCode } = await createOrder(cart);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error("Failed to create order:", error);
+        res.status(500).json({ error: "Failed to create order." });
+    }
+});
+
+const captureOrder = async (orderID) => {
+    const collect = {
+        id: orderID,
+        prefer: "return=minimal",
+    };
+
+    try {
+        const { body, ...httpResponse } = await ordersController.captureOrder(
+            collect
+        );
+        return {
+            jsonResponse: JSON.parse(body),
+            httpStatusCode: httpResponse.statusCode,
+        };
+    } catch (error) {
+        if (error instanceof ApiError) {
+            throw new Error(error.message);
+        }
+    }
+};
+
+app.post("/api/orders/:orderID/capture", async (req, res) => {
+    try {
+        const { orderID } = req.params;
+        const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error("Failed to create order:", error);
+        res.status(500).json({ error: "Failed to capture order." });
+    }
+});
+
+const authorizeOrder = async (orderID) => {
+    const collect = {
+        id: orderID,
+        prefer: "return=minimal",
+    };
+
+    try {
+        const { body, ...httpResponse } = await ordersController.authorizeOrder(
+            collect
+        );
+        return {
+            jsonResponse: JSON.parse(body),
+            httpStatusCode: httpResponse.statusCode,
+        };
+    } catch (error) {
+        if (error instanceof ApiError) {
+            throw new Error(error.message);
+        }
+    }
+};
+
+app.post("/api/orders/:orderID/authorize", async (req, res) => {
+    try {
+        const { orderID } = req.params;
+        const { jsonResponse, httpStatusCode } = await authorizeOrder(orderID);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error("Failed to create order:", error);
+        res.status(500).json({ error: "Failed to authorize order." });
+    }
+});
+
+const captureAuthorize = async (authorizationId) => {
+    const collect = {
+        authorizationId: authorizationId,
+        prefer: "return=minimal",
+        body: {
+            finalCapture: false,
+        },
+    };
+    try {
+        const { body, ...httpResponse } =
+            await paymentsController.captureAuthorize(collect);
+        return {
+            jsonResponse: JSON.parse(body),
+            httpStatusCode: httpResponse.statusCode,
+        };
+    } catch (error) {
+        if (error instanceof ApiError) {
+            throw new Error(error.message);
+        }
+    }
+};
+
+app.post("/orders/:authorizationId/captureAuthorize", async (req, res) => {
+    try {
+        const { authorizationId } = req.params;
+        const { jsonResponse, httpStatusCode } = await captureAuthorize(
+            authorizationId
+        );
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error("Failed to create order:", error);
+        res.status(500).json({ error: "Failed to capture authorize." });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Node server listening at http://localhost:${PORT}/`);
+});
